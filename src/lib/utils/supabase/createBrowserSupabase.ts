013294ace65b25e39f95be02e4a26f646b31363e
RomanTosakov <roman@plaini.io>
2024-10-14 18:23:42 +0200

    feat: add Playwright testing setup and configuration; update Supabase client types

--- src/lib/utils/supabase/createBrowserSupabase.ts
+++ src/lib/utils/supabase/createBrowserSupabase.ts
@@ -1,7 +1,8 @@
+import { TSupabaseClient } from '@/lib/types/api'
 import { createBrowserClient } from '@supabase/ssr'
 
 export const createBrowserSupabase = () => {
-  const supabase = createBrowserClient(
+  const supabase = createBrowserClient<TSupabaseClient>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )

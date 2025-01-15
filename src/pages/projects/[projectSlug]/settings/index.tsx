8e50d02a877dcf33f2035e46a55fccca4f8d59a9
Illia_Nieniunkin <Illia_Nieniunkin>
2025-01-15 11:18:25 +0100

    feat: update project slug handling in DetailsView component

---
+++ src/pages/projects/[projectSlug]/settings/index.tsx
@@ -0,0 +1,18 @@
+import { GetServerSidePropsContext } from 'next'
+
+const Page = () => {
+  return <></>
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { params } = context
+
+  return {
+    redirect: {
+      destination: `/projects/${params?.projectSlug}/settings/details`,
+      permanent: false
+    }
+  }
+}
+
+export default Page
